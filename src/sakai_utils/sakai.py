56cc2d1bfa408576043f180e9f19a799977082ab
tanashou <tanashou0210@gmail.com>
2024-04-24 23:28:38 +0900

    Refactor Sakai utility class to improve code readability and maintainability

--- src/sakai_utils/sakai.py
+++ src/sakai_utils/sakai.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from requests.utils import cookiejar_from_dict
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
@@ -12,15 +13,32 @@ from typing import Any
 
 
 class Sakai:
-    def __init__(self) -> None:
-        pass
+    SITE_COLLECTION_JSON_URL = "https://tact.ac.thers.ac.jp/direct/site.json"
 
-    def __wait_for_page_load(self, driver: webdriver.Chrome) -> Any:
-        return WebDriverWait(driver, 10).until(
-            lambda d: d.execute_script("return document.readyState") == "complete"  # type: ignore
-        )
+    def __init__(self) -> None:
+        self.set_cookiejar()
+
+    def set_cookiejar(self) -> None:
+        driver = None
+        selenium_cookies = None
+        try:
+            driver = self.login()
+            selenium_cookies = driver.get_cookies()
+        except TimeoutException as e:
+            print("タイムアウトしました。もう一度試してください。")
+            raise e
+        finally:
+            if driver is not None:
+                driver.quit()
+        cookie_dict = {cookie["name"]: cookie["value"] for cookie in selenium_cookies}
+        self.cookie_jar = cookiejar_from_dict(cookie_dict)  # type: ignore
 
     def login(self) -> webdriver.Chrome:
+        def wait_for_page_load(driver: webdriver.Chrome) -> Any:
+            return WebDriverWait(driver, 10).until(
+                lambda d: d.execute_script("return document.readyState") == "complete"  # type: ignore
+            )
+
         USERNAME = os.environ.get("TACT_USERNAME")
         PASSWORD = os.environ.get("TACT_PASSWORD")
 
@@ -42,19 +60,19 @@ class Sakai:
         )
         driver.get(login_url)
 
-        self.__wait_for_page_load(driver)
+        wait_for_page_load(driver)
         email_input = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, EMAIL_INPUT_BOX_ID))
         )
         email_input.send_keys(USERNAME)
 
-        self.__wait_for_page_load(driver)
+        wait_for_page_load(driver)
         next_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, CONFIRM_BUTTON_ID))
         )
         next_button.click()
 
-        self.__wait_for_page_load(driver)
+        wait_for_page_load(driver)
         password_input = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, PASSWORD_INPUT_BOX_ID))
         )
@@ -64,7 +82,7 @@ class Sakai:
         )
         sign_in_button.click()
 
-        self.__wait_for_page_load(driver)
+        wait_for_page_load(driver)
         otp_input = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, ONETIME_PASSWORD_INPUT_BOX_ID))
         )
@@ -72,55 +90,39 @@ class Sakai:
             input("ワンタイムパスワードを入力してください:\n>")
         )  # TODO: OTPを生成して入力する手間を省きたい
 
-        self.__wait_for_page_load(driver)
+        wait_for_page_load(driver)
         verify_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, VERIFY_BUTTON_ID))
         )
         verify_button.click()
 
-        self.__wait_for_page_load(driver)
+        wait_for_page_load(driver)
         stay_signed_in_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, CONFIRM_BUTTON_ID))
         )
         stay_signed_in_button.click()
 
-        self.__wait_for_page_load(driver)
+        wait_for_page_load(driver)
         accept_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, ACCEPT_BUTTON_XPATH))
         )
         accept_button.click()
 
-        self.__wait_for_page_load(driver)
+        wait_for_page_load(driver)
 
         return driver
 
-    def session_with_cookie(self, driver: webdriver.Chrome) -> requests.Session:
-        cookies = driver.get_cookies()
+    def create_session(self) -> requests.Session:
         session = requests.Session()
-        for cookie in cookies:
-            session.cookies.set(cookie["name"], cookie["value"])
-
+        session.cookies.update(self.cookie_jar)
         return session
-
-    def perform_login_and_get_session(self) -> requests.Session:
-        driver = None
-        try:
-            driver = self.login()
-            session = self.session_with_cookie(driver)
-            return session
-        except TimeoutException as e:
-            print("タイムアウトしました。もう一度試してください。")
-            raise e
-        finally:
-            if driver is not None:
-                driver.quit()
 
 
 portal_url = "https://tact.ac.thers.ac.jp/portal"
 
 if __name__ == "__main__":
     sakai = Sakai()
-    session = sakai.perform_login_and_get_session()
+    session = sakai.create_session()
     test_url = "https://tact.ac.thers.ac.jp/direct/site.json"
     response = session.get(test_url)
     print(response.text)
