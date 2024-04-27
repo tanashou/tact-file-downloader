d474bbc3d57db8d73950b7e26e8efdb0c47651b8
tanashou <tanashou0210@gmail.com>
2024-04-27 16:40:15 +0900

    Refactor SakaiContent class to update the path attribute based on the site_title

--- src/tact_file_downloader/sakai_content.py
+++ src/tact_file_downloader/sakai_content.py
@@ -3,7 +3,13 @@ from dataclasses import dataclass
 
 @dataclass
 class SakaiContent:
+    container: str  # "\/content\/group\/{site_id}\/{directory_name}\/{sub_directory_name_and_so_on}",
     title: str
-    type: str
+    type: str # if type is "container" then it is a directory. otherwise it is a file
     url: str
-    size: int
+    site_title: str
+
+    def __post_init__(self):
+        path_parts = self.container.split("/")
+        path_parts[2] = self.site_title # replace the site_id with the site_title
+        self.path = "/".join(path_parts[2:]) # remove the first two parts of the path
